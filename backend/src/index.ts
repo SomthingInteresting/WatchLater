486a2f667623b35e14db37c27c6cb7b5df29a4ac
Quddus Rahman <qrahman115@gmail.com>
2023-07-18 12:21:50 +0100

    added prod and test mongo db to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
 
+const prodDb = 'mongodb://localhost:27017/WatchLater';
+const testDb = 'mongodb://localhost:27017/WatchLater_test';
+
+// Connect to MongoDB
+mongoose.connect(process.env.NODE_ENV === 'test' ? testDb : prodDb)
+  .then(() => {
+    console.log('Connected to the database');
+  }).catch((error) => {
+    console.error('Error connecting to the database', error);
+  });
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
