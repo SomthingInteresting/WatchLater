585bc9ce9fc6449469cbcf0e9d1c3897f292e4f4
Quddus Rahman <qrahman115@gmail.com>
2023-07-19 09:34:36 +0100

    passing test for create movie and save to testdb

--- backend/src/__tests__/movie.test.ts
+++ backend/src/__tests__/movie.test.ts
@@ -1,13 +1,44 @@
-import { Movie } from '../models/Movie';
+import { Movie, IMovie } from '../models/Movie';
+import mongoose from 'mongoose';
 
-test('it should be able to create a new movie', () => {
-    const movie = new Movie({
-        title: 'Inception',
-        watched: false,
-        userId: 'testuserid'
+describe('Movie Model Test', () => {
+    beforeAll(async () => {
+        const url = `mongodb://localhost/WatchLater_test`;
+        await mongoose.connect(url);
+    });      
+
+    afterAll(async () => {
+        await mongoose.connection.close();
     });
 
-    expect(movie.title).toBe('Inception');
-    expect(movie.watched).toBe(false);
-    expect(movie.userId).toBe('testuserid');
+    afterEach(async () => {
+        await Movie.deleteMany();
+    });
+
+    test('it should be able to create a new movie', () => {
+        const movie = new Movie({
+            title: 'Inception',
+            watched: false,
+            userId: 'testuserid'
+        });
+
+        expect(movie.title).toBe('Inception');
+        expect(movie.watched).toBe(false);
+        expect(movie.userId).toBe('testuserid');
+    });
+
+    it('create & save movie successfully', async () => {
+        const movieData = {
+            title: 'Inception',
+            watched: false,
+            userId: 'testuserid'
+        };
+        const validMovie = new Movie(movieData);
+        const savedMovie: IMovie = await validMovie.save();
+
+        expect(savedMovie._id).toBeDefined();
+        expect(savedMovie.title).toBe(movieData.title);
+        expect(savedMovie.watched).toBe(movieData.watched);
+        expect(savedMovie.userId).toBe(movieData.userId);
+    });
 });
