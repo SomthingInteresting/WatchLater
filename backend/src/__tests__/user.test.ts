11a9ca8468b8607eafe2e95a3335e65e2d0f3f53
Quddus Rahman <qrahman115@gmail.com>
2023-07-19 09:30:35 +0100

    passing test for create user and and save to database confirming testdb working

--- backend/src/__tests__/user.test.ts
+++ backend/src/__tests__/user.test.ts
@@ -1,6 +1,21 @@
-import { User } from '../models/User';
+import { User, IUser } from '../models/User';
+import mongoose from 'mongoose';
 
-test('it should be able to create a new user', () => {
+describe('User Model Test', () => {
+    beforeAll(async () => {
+        const url = `mongodb://localhost/WatchLater_test`;
+        await mongoose.connect(url);
+    });      
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  afterEach(async () => {
+    await User.deleteMany();
+  });
+
+  test('it should be able to create a new user', () => {
     const user = new User({
         username: 'testuser',
         email: 'testuser@example.com',
@@ -10,4 +25,20 @@ test('it should be able to create a new user', () => {
     expect(user.username).toBe('testuser');
     expect(user.email).toBe('testuser@example.com');
     expect(user.password).toBe('password123');
+  });
+
+  it('create & save user successfully', async () => {
+    const userData = {
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'password123'
+    };
+    const validUser = new User(userData);
+    const savedUser: IUser = await validUser.save();
+
+    expect(savedUser._id).toBeDefined();
+    expect(savedUser.username).toBe(userData.username);
+    expect(savedUser.email).toBe(userData.email);
+    expect(savedUser.password).toBe(userData.password);
+  });
 });
